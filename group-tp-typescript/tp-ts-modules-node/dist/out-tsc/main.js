e7b329f263870fe8219cf84ecebe6e647bf1d2ec
didier <didier@d-defrance.fr>
2020-05-06 10:25:01 +0200

    avec Stat et export/import

---
+++ group-tp-typescript/tp-ts-modules-node/dist/out-tsc/main.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//importer Serie depuis Serie(.ts)
+//importer Stat et buildStatFromValues depuis Stats(.ts)
+var Serie_1 = require("./Serie");
+var Stats_1 = require("./Stats");
+function myMainFunction() {
+    var serie1 = new Serie_1.Serie("serie1", [2, 6, 8]);
+    serie1.push(4);
+    serie1.push(10);
+    console.log(JSON.stringify(serie1));
+    var statForSerie1;
+    statForSerie1 = Stats_1.buildStatFromValues(serie1.values);
+    console.log("moyenne =" + statForSerie1.average);
+}
+myMainFunction();
+//# sourceMappingURL=main.js.map
---
+++ group-tp-typescript/tp-ts-modules-node/src/main.ts
@@ -0,0 +1,15 @@
+//importer Serie depuis Serie(.ts)
+//importer Stat et buildStatFromValues depuis Stats(.ts)
+import { Serie } from './Serie';
+import { Stat , buildStatFromValues } from './Stats';
+
+function myMainFunction(){
+    let serie1 = new Serie("serie1",[2,6,8]);
+    serie1.push(4);serie1.push(10);
+    console.log(JSON.stringify(serie1));
+    let statForSerie1 : Stat;
+    statForSerie1 = buildStatFromValues(serie1.values);
+    console.log("moyenne ="+statForSerie1.average)
+    }
+
+myMainFunction();
